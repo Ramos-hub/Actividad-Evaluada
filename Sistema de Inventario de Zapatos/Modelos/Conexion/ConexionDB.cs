99b4ff7ce7981b9ccfef6ae5bb380ac348aa1249
unknown <20220349@ricaldone.edu.sv>
2025-08-14 15:36:53 -0600

    falta completar

---
+++ Sistema de Inventario de Zapatos/Modelos/Conexion/ConexionDB.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Modelos.Conexion
+{
+    public class ConexionDB
+    {
+        private static string servidor = "LAB03-DS-EQ08\\SQLEXPRESS";
+        private static string dbdata = "ZapatosDB";
+
+        public static SqlConnection Conectar()
+        {
+            try
+            {
+                string cadena = $"Data source= {servidor};Initial Catalog {dbdata};Integrated security=true;";
+                SqlConnection Conexion = new SqlConnection(cadena);
+                Conexion.Open();
+                return Conexion;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show("No se pudo conectar al servidor"+ex,
+                    "Error de conexion", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+
+            }
+        }
+    }
+}
---
+++ Sistema de Inventario de Zapatos/Modelos/Entidades/Zapato.cs
@@ -0,0 +1,30 @@
+﻿using Modelos.Conexion;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos.Entidades
+{
+    public class Zapato
+    {
+        private int idCategoria;
+        private string nombre;
+
+        public int IdCategoria { get => idCategoria; set => idCategoria = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+
+        public static DataTable cargarCategoria()
+        {
+            SqlConnection conexion = ConexionDB.Conectar();
+            string consultaQuery = "select Id, Nombre from Categorias";
+            SqlDataAdapter add = new SqlDataAdapter(consultaQuery, conexion);
+            DataTable tablaCarga = new DataTable();
+            add.Fill(tablaCarga);
+            return tablaCarga;
+        }
+    }
+}
